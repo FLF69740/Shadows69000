03b6a03758e6371ad958cc42a3f5de7b24bc0382
LE FORT François <slayer171.flf@gmail.com>
2024-04-18 11:56:06 +0200

    Create landing box for body

---
+++ shadows/src/app/information-area-landing/information-area-landing.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-information-area-landing',
+  standalone: true,
+  imports: [],
+  templateUrl: './information-area-landing.component.html',
+  styleUrl: './information-area-landing.component.scss'
+})
+export class InformationAreaLandingComponent {
+
+}
