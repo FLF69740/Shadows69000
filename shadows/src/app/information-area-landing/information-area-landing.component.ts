70ac08c6f618fb10ec083dc34fba97b00615ab8d
LE FORT François <slayer171.flf@gmail.com>
2024-04-19 12:09:31 +0200

    Create landing content

--- shadows/src/app/information-area-landing/information-area-landing.component.ts
+++ shadows/src/app/information-area-landing/information-area-landing.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { InformationLineAreaLandingComponent } from '../information-line-area-landing/information-line-area-landing.component';
+import { LineInformationLanding } from '../model/line-information-landing.model';
 
 @Component({
   selector: 'app-information-area-landing',
   standalone: true,
-  imports: [],
+  imports: [InformationLineAreaLandingComponent],
   templateUrl: './information-area-landing.component.html',
   styleUrl: './information-area-landing.component.scss'
 })
-export class InformationAreaLandingComponent {
+export class InformationAreaLandingComponent implements OnInit {
+
+  linesInformation!: LineInformationLanding[];
+
+  ngOnInit(): void {
+    this.linesInformation = [
+      {
+        picture: "/assets/pictures/amarr.png",
+        name: "AMARR"
+      },
+      {
+        picture: "/assets/pictures/sons_of_sylph.png",
+        name: "SONS OF SYLPH"
+      },
+      {
+        picture: "/assets/pictures/cva.png",
+        name: "CVA"
+      }
+    ];
+  }
+
+
 
 }
